7d91b607b7eed53f213659f5386918014f8f7fec
Adrian Melo <amelo2@fvi.edu>
2017-03-30 21:52:54 -0400

    finish quiz2

--- app/BlogPostCtrl.js
+++ app/BlogPostCtrl.js
@@ -1,11 +1,24 @@
 (function ()
 {
     'use strict';
-    function BlogPostCtrl()
+    function BlogPostCtrl($scope)
     {
+      $scope.post = [
+        {
+        'id': 1,
+        'author': "Jack",
+        'title': "Diving Deep with Dependency Injection"
+      },
+        {
+        'id': 2,
+        'author': "Jill",
+        'title': "Practical End-to-End Testing with Protractor"
+      }
+    ];
 
     }
 
+
     var module = angular.module("exerciseApp", []);
-    module.controller('BlogPostCtrl', [BlogPostCtrl]);
+    module.controller('BlogPostCtrl', ['$scope', BlogPostCtrl]);
 })();
